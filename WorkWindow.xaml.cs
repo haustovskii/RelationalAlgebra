d3718accbbccad9298c8665a433f83616fe1b290
haustovskii <haus-tov@mail.ru>
2023-10-30 14:00:50 +0300

    Добавьте файлы проекта.

---
+++ WorkWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RelationalAlgebra
+{
+    /// <summary>
+    /// Логика взаимодействия для WorkWindow.xaml
+    /// </summary>
+    public partial class WorkWindow : Window
+    {
+        public WorkWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
+        }
+        private void ImgClose_MouseDown(object sender, RoutedEventArgs e) => Close();
+        private void ImgPollUp_MouseDown(object sender, RoutedEventArgs e) => WindowState = WindowState.Minimized;
+    }
+}
